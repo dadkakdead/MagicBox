21fa2a811704fb72997276bfcb9e315853f12121
devrazdev <devrazdev@gmail.com>
2018-08-24 21:41:24 +0000

    Fixed garmin script for ambiguous date

--- webrequest/scripts/garmin_script.py
+++ webrequest/scripts/garmin_script.py
@@ -12,6 +12,10 @@ def print_time(t):
     t_aware = convert_time(t)
     return t_aware.astimezone(pytz.timezone('Etc/GMT+7')).strftime("%H:%M:%S")
 
+def print_date(t):
+    t_aware = convert_time(t)
+    return t_aware.astimezone(pytz.timezone('Etc/GMT+7')).strftime("%d %b %Y")
+
 def convert_to_binary_string(n):
     return ''.join(str(1 & int(n) >> i) for i in range(0, 32)[::-1])
 
@@ -30,6 +34,8 @@ def process_data_csv(df_in):
 
     df_hr["time"] = list(map(print_time, df_hr["timestamp"]))
 
+    df_hr["date"] = list(map(print_date, df_hr["timestamp"]))
+
     df_hr["heart_rate[bpm]"] = df_hr['monitoring.heart_rate[bpm]']
 
     return df_hr
@@ -39,7 +45,7 @@ def add_minutes(df_in):
     df_out["minutes"] = 0
 
     for i in range(1,len(df_out)):
-        df_out.at[i,"minutes"] = math.ceil((convert_time(df_in.at[i,"timestamp"]) - convert_time(df_in.at[0,"timestamp"])).total_seconds() / 60)
+        df_out.at[i,"minutes"] = math.ceil((convert_time(df_in.at[i,"timestamp"]) - convert_time(df_in.at[0,"timestamp"])).total_seconds() / 60) + 1
 
     return df_out
 
@@ -61,10 +67,13 @@ def create_report(reportKey, pathToZip):
         except:
             pass
 
-    #reshape the output
-    df_out = add_minutes(df_out.sort_values(by=['timestamp']).reset_index(drop=True))
-    df_out = df_out[['time', 'minutes', 'heart_rate[bpm]']][df_out["minutes"] > 0][df_out["heart_rate[bpm]"] > 0].drop_duplicates().reset_index(drop=True)
+    #sometimes data point from previous day occur in data series. we filter them out statistically
+    from collections import Counter
+    most_frequent_date = Counter([v[0] for v in df_out[['date']].values]).most_common(1)[0][0]
 
+    #reshape the output
+    df_out = add_minutes(df_out[df_out["date"] == most_frequent_date].sort_values(by=['timestamp']).reset_index(drop=True))
+    df_out = df_out[['time', 'minutes', 'heart_rate[bpm]']][df_out["minutes"] > 0][df_out["heart_rate[bpm]"] > 0].drop_duplicates().reset_index(drop=True)
 
     # write report
     reportPath = write_report(df_out, "output")
